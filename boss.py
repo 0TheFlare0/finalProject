d4f51b2fe399b8685d0f2cb3718936dbde5342a6
twoney-money <37705263+twoney-money@users.noreply.github.com>
2018-05-01 13:32:28 -0700

    Add files via upload
    The new boss to allow automatic following

--- boss.py
+++ boss.py
@@ -1,36 +1,58 @@
+#adapted from cozort
 import pygame
-
-class Boss():
-  
-  
-
-#--------------------------
+from pygame.sprite import Sprite
 
 
-import sys, random, pygame
-from pygame.locals import *
-pygame.init()
+class Enemy(Sprite):
+    # a class to represent an enemy ship
 
-WIDTH = 1200 #game window width
-HEIGHT = 800 #game window height
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) #set the game window
+    def __init__(self, ai_settings, screen):
+        # init the alien and set its starting pos
+        super(Enemy, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the enemy image and set its rect attribute
+        self.image = pygame.image.load('Boss.png')
+        self.rect = self.image.get_rect()
+
+        # start each enemy at the middle right screen
+        self.rect.midleft = self.screen_rect.midright
+
+        def blitme(self):
+            """Draw the Boss at its current location."""
+            self.screen.blit(self.image, self.rect)
+
+        # store aliens exact position
+        self.y = float(self.rect.y)
+
+    def update(self):
+        # move alien up/down
+        self.y += (self.ai_settings.enemy_speed_factor * self.ai_settings.boss_direction)
+        self.rect.y = self.y
 
 
-#comes from cozort canvas
-#bos dictionary
- player1 = {
-'name': 'Boss',
-'hitPoints': 300,
-'alive':True
-}
+        # Taken from https://stackoverflow.com/questions/20044791/how-to-make-an-enemy-follow-the-player-in-pygame written by Ashish Nitin Patil
 
-#https://stackoverflow.com/questions/30015787/random-movement-pygame
-class Boss:
-    def __init__(self):
-        self.y = random.randrange(30, HEIGHT-30) #y position
-        self.speed = random.randrange(1,4) #cell speed
+    class Enemy(object):
+        def __init__(self, y):  # initial position
+            self.y = y
 
-    brick_tile = pygame.image.load('boss.png')
+        def move(self, speed=5):  # chase movement
+            # Movement along y direction
+            if self.y < py:
+                self.y += speed
+            elif self.y > py:
+                self.y -= speed
 
+    def check_edges(self):
+        # return true if boss is at top/bottom of screen
+        screen_rect = self.screen.get_rect()
+        if self.rect.top >= screen_rect.top:
+            return True
+        elif self.rect.bottom <= 0:
+            return True
 
-mainloop()
+    def blitme(self):
+        # draw alien at its current location
+        self.screen.blit(self.image, self.rect)
